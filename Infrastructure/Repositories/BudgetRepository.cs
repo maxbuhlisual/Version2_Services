a02708417a96f1f936db8ef3c8a4cfbb008d7046
manuelat <manuelat@student.21-school.ru>
2026-07-30 17:47:27 +0300

    BudgetRepository.cs

---
+++ Infrastructure/Repositories/BudgetRepository.cs
@@ -0,0 +1,36 @@
+using Domain.Models;
+namespace Infrastructure.Repositories;
+
+public class BudgetRepository
+{
+    public void Add(Budget budget)
+    {
+        InMemoryDatabase.Budgets[budget.Id] =  budget;
+    }
+    
+    public List<Budget> GetAllByUser(Guid userId)
+    {
+        List<Budget> result = new();
+        foreach (Budget budget in InMemoryDatabase.Budgets.Values)
+        {
+            if (budget.UserId == userId)
+            {
+                result.Add(budget);
+            }
+        }
+        return result;
+    }
+    
+    public Budget? GetByUserCategoryAndPeriod(Guid userId,Guid categoryId, int year, int month)
+    {
+        foreach (Budget budget in InMemoryDatabase.Budgets.Values)
+        {
+            if (budget.UserId == userId && budget.CategoryId == categoryId 
+                                        && budget.Year == year && budget.Month == month)
+            {
+                return budget;
+            }
+        }
+        return null;
+    }
+}
