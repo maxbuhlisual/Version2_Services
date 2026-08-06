bb5691f8b1a62c390ee135c851e65d6881ca934e
manuelat <manuelat@student.21-school.ru>
2026-08-06 12:40:21 +0300

    Program.cs

--- ExpenseTracker.Worker/Program.cs
+++ ExpenseTracker.Worker/Program.cs
@@ -1,4 +1,18 @@
+using Domain.Interfaces;
+using Infrastructure.Repositories;
+using Application.Services;
+
 var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddScoped<IBudgetRepository, BudgetRepository>();
+builder.Services.AddScoped<IExpenseRepository, ExpenseRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+
+builder.Services.AddScoped<UserService>();
+builder.Services.AddScoped<ExpenseService>();
+builder.Services.AddScoped<CategoryService>();
+builder.Services.AddScoped<BudgetService>();
 
 var host = builder.Build();
 host.Run();
